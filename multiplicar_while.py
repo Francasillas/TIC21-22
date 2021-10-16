b8bf09d230d9dd5f81ae5d341446dce6aaca9832
Francasillas <90762319+Francasillas@users.noreply.github.com>
2021-10-16 15:49:26 +0200

    Add files via upload

---
+++ multiplicar_while.py
@@ -0,0 +1,7 @@
+def tabla_multiplicar():
+    numero=input("Que tabla deseas escribir: ")
+    print("TABLA DEL "+str(numero))
+    cont=0
+    while(cont<11):
+        print(str(numero+ " x "+ str(cont) + ("= ")+str(num*cont))
+tabla_multiplicar()
