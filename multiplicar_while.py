23eb5c61304fdf888c97b46f9336a9b8a2b390ea
Francasillas <90762319+Francasillas@users.noreply.github.com>
2021-10-16 16:04:08 +0200

    Delete multiplicar_while.py

--- multiplicar_while.py
+++
@@ -1,7 +0,0 @@
-def tabla_multiplicar():
-    numero=input("Que tabla deseas escribir: ")
-    print("TABLA DEL "+str(numero))
-    cont=0
-    while(cont<11):
-        print(str(numero+ " x "+ str(cont) + ("= ")+str(num*cont))
-tabla_multiplicar()
