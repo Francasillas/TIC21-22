fa7075b8f70a9e8f1fe2f2059ba6794a4cc3ac1d
Francasillas <90762319+Francasillas@users.noreply.github.com>
2021-11-24 19:27:00 +0100

    Add files via upload

---
+++ cuadrado1.py
@@ -0,0 +1,16 @@
+def cuadrado1():
+    linea=" "
+    fil=" "
+    num=input("dame un numero: ")
+    num=int(num)
+    for fil in range(1,num+1):
+        if(fil%2==1):
+            letra="1"
+        else:
+            letra="0"
+        for col in range(1,num+1):
+            linea=linea+letra
+        print(linea)
+        linea=" "
+
+cuadrado1()
