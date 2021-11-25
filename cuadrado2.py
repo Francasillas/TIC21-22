cb37be60ae86c699c120a4e758b5fccd9f114c79
Francasillas <90762319+Francasillas@users.noreply.github.com>
2021-11-25 20:38:40 +0100

    Add files via upload

---
+++ cuadrado2.py
@@ -0,0 +1,15 @@
+def cuadrado2():
+    fila=" "
+    num=input("Dame un numero: ")
+    for fil in range(1,num+1):
+        for col in range(1,num+1):
+            if (fil%2==0 and col%2==1):
+                fila=fila+"0"
+            else:
+                if(fil%2==1 and col%2==0):
+                    fila=fila+"0"
+                else:
+                    fila=fila+"1"
+        print(fila)
+        fila=" "
+cuadrado2()
