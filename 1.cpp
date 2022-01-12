f07bd41663bcaffe02613456db60d87ebb37431c
Francasillas <90762319+Francasillas@users.noreply.github.com>
2022-01-12 18:54:46 +0100

    Create 1.cpp

---
+++ 1.cpp
@@ -0,0 +1,12 @@
+// ESTO ES UN COMENTARIO
+/*ESTO ES COMENTARIO DE MÁS LÍNEAS*/
+//INSTRUCCIONES AL PREPROCESADOR//
+#include <stdio.h>//FICHERO DE CABECERA
+int main(){
+    char salir;//declarar variable//
+    printf("hola mundo cruel");
+    printf("\nPresione cualquier tecla para salir: ");
+    scanf("%c",&salir);
+    return 0;
+
+}
